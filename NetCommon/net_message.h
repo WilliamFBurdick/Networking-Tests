1ff030f917b752fd26e48ab81389cf9d651b8797
WilliamFBurdick <williamfburdick@gmail.com>
2026-06-24 02:50:35 -0400

    Asynchronous I/O with server Creation of networking common types

---
+++ NetCommon/net_message.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "net_common.h"
+
+namespace networking
+{
+	template <typename T>
+	struct message_header
+	{
+		T id{};
+		uint32_t size = 0;
+	};
+
+	template <typename T>
+	struct message
+	{
+		message_header<T> header{};
+		std::vector<uint8_t> body;
+
+		size_t size() const
+		{
+			return sizeof(message_header<T>) + body.size();
+		}
+	};
+}
--- SimpleExample/SimpleExample.cpp
+++ SimpleExample/SimpleExample.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 
 #ifdef _WIN32
@@ -9,12 +10,36 @@
 #include <asio/ts/buffer.hpp>
 #include <asio/ts/internet.hpp>
 
+std::vector<char> vBuffer(20 * 1024);
+
+void GrabSomeData(asio::ip::tcp::socket& socket)
+{
+	socket.async_read_some(asio::buffer(vBuffer.data(), vBuffer.size()),
+		[&](std::error_code ec, std::size_t length)
+		{
+			if (!ec)
+			{
+				std::cout << "\nnRead " << length << " bytes\n\n";
+				for (int i = 0; i < length; i++)
+					std::cout << vBuffer[i];
+				GrabSomeData(socket);
+			}
+		}
+	);
+}
+
 int main(int argc, char** argv)
 {
 	asio::error_code ec;
 
 	// Create a "context" - a platform specific interface
 	asio::io_context context;
+
+	// Create a work guard, to prevent the context from completing early.
+	auto workGuard = asio::make_work_guard(context);
+
+	// Start the context on its own thread, to prevent it from blocking the main application.
+	std::thread thrContext = std::thread([&]() {context.run(); });
 
 	// Get the address of somewhere we wish to connect to.
 	asio::ip::tcp::endpoint endpoint(asio::ip::make_address("51.38.81.49", ec), 80);
@@ -34,25 +59,20 @@ int main(int argc, char** argv)
 
 	if (socket.is_open())
 	{
+		GrabSomeData(socket);
+
 		std::string sRequest =
 			"GET /index.html HTTP/1.1\r\n"
 			"Host: example.com\r\n"
 			"Connection: close\r\n\r\n";
 
 		socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-		socket.wait(socket.wait_read);
 
-		size_t bytes = socket.available();
-		std::cout << "Bytes Available: " << bytes << std::endl;
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(20000ms);
 
-		if (bytes > 0)
-		{
-			std::vector<char> vBuffer(bytes);
-			socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-
-			for (auto c : vBuffer)
-				std::cout << c;
-		}
+		context.stop();
+		if (thrContext.joinable()) thrContext.join();
 	}
 
 	return 0;
